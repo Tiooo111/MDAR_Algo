5f98514e49ffac8a91c07a6d624da3d3b0487da4
Tiooo111 <33785995@qq.com>
2020-08-18 20:15:01 +0800

    update

--- 1.Special/headFile.cpp
+++ 1.Special/headFile.cpp
@@ -6,6 +6,20 @@
 #include <vector>
 #include <cmath>
 
+#define LOWBIT(i) (i)&(-i)
+#define REP(i,k) for(int i=0;i<(k);++i)
+#define FOR(i,j,k) for(int i=(j);i<(k);++i)
+#define MP make_pair
+#define PB push_back
+#define PP pop_back
+#define SIZE(A) ((int)A.size())
+#define ALL(A) A.begin(),A.end()
+#define Parent(i) ((i)>>1)
+#define LChild(i) ((i)<<1)
+#define RChild(i) ((i)<<1|1)
+#define ch() getchar()
+#define pc(x) putchar(x)
+
 template<typename T>inline T read(){
     int f, x;char c;
     for(f=1,c=ch();!isdigit(c);c=ch())if(c=='-')f=-f;
@@ -22,25 +36,13 @@ template<class T> inline T  Omin(T &a,T &b) {return a=min(a,b);}
 template<class T> inline T  Omax(T &a,T &b) {return a=max(a,b);}
 template<class T> inline T  Osqr(T a) {return sqrt((a));}
 
-#define LOWBIT(i) (i)&(-i)
-#define REP(i,k) for(int i=0;i<(k);++i)
-#define FOR(i,j,k) for(int i=(j);i<(k);++i)
-#define MP make_pair
-#define PB push_back
-#define PP pop_back
-#define SIZE(A) ((int)A.size())
-#define ALL(A) A.begin(),A.end()
-#define Parent(i) ((i)>>1)
-#define LChild(i) ((i)<<1)
-#define RChild(i) ((i)<<1|1)
-
 using uint=unsigned int;
 using int64=long long;
 using uint64=unsigned long long;
-using ipair=pair<int,int>;
-using VI=vector<int>;
-using VD=vector<double>;
-using VVI=vector<VI>;
+using ipair=std::pair<int,int>;
+using VI=std::vector<int>;
+using VD=std::vector<double>;
+using VVI=std::vector<VI>;
 
 const double pi=acos(-1.0);
 const double eps=1e-11;
@@ -55,6 +57,6 @@ int main()
 	//========================================================
 	
 	//========================================================
-	std::cess << "Time:" << clock() - stime << std::endl; 
+	std::cerr << "Time:" << clock() - stime << std::endl; 
 	return 0;
 }
--- 1.Special/init.sh
+++ 1.Special/init.sh
@@ -1,8 +1,8 @@
 ##########################################################################
-# File Name: prepare.sh
+# File Name: init.sh
 # Author: Tiooo111
 # mail: tiooo111@163.com
-# Created Time: 2020年08月18日 星期二 12时18分27秒
+# Created Time: 2020年08月18日 星期二 19时50分27秒
 #########################################################################
 #!/bin/zsh
 PATH=/home/edison/bin:/home/edison/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin:/work/tools/gcc-3.4.5-glibc-2.3.6/bin
@@ -13,23 +13,20 @@ MDAR_Algo_headFile="${MDAR_Algo}/1.Special/headFile.cpp";
 
 script_path=$(cd "$(dirname "$0")" && pwd);
 
-init(){
-    echo "Please run init.sh";
-	exit 2;
+init_MDAR(){
+    echo "将从git仓库拉取MDAR";
 }
 
-if [ ! -d "$MDAR_Algo" -o ! -f "/bin/prepare.sh" ];then
-	init;
+if [ ! -d "$MDAR_Algo" ];then
+	init_MDAR;
 fi
 
-if [ ${#} -lt 1 ];then
-	echo "Please pass in the path parameter";
-	exit 3;
+if [ ! -f "/bin/prepare.sh" ];then
+	sudo cp "${script_path}/prepare.sh" "/bin/prepare.sh";
+	sudo chmod 755 "/bin/prepare.sh";
+	echo "init prepare.sh complete!"
 fi
 
-read -p "You are sure you wang to prepare?[y/n]" input;
+exit 1;
 
-if [ $input = "y" ];then
-	cat ${MDAR_Algo_headFile} > ${1};
-fi
 
