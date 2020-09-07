4288423a91b5e19dc3dbdc112544b8c94409f1c1
Tiooo111 <33785995@qq.com>
2020-09-07 19:40:51 +0800

    creat inv

---
+++ 6.Math/inv/p3811/p3811.cpp
@@ -0,0 +1,72 @@
+//#define LOCAL
+#include <iostream>
+#include <cstdio>
+#include <ctime>
+#include <cctype>
+#include <vector>
+#include <cmath>
+
+#define OMID (r-l>>1)+l
+#define LOWBIT(i) (i)&(-i)
+#define REP(i,k) for(int i=0;i<(k);++i)
+#define FOR(i,j,k) for(int i=(j);i<(k);++i)
+#define MP make_pair
+#define PB push_back
+#define PP pop_back
+#define SIZE(A) ((int)A.size())
+#define ALL(A) A.begin(),A.end()
+#define PA ((idx)>>1)
+#define LC ((idx)<<1)
+#define RC ((idx)<<1|1)
+#define ch() getchar()
+#define pc(x) putchar(x)
+
+template<typename T>inline T read(){
+    int f, x;char c;
+    for(f=1,c=ch();!isdigit(c);c=ch())if(c=='-')f=-f;
+    for(x=0;isdigit(c);c=ch())x=x*10+(c&15);
+	return x*f;
+}
+template<typename T>inline void print(T x){
+    static char q[64];int cnt=0;
+    if(!x)pc('0');if(x<0)pc('-'),x=-x;
+    while(x)q[cnt++]=x%10+'0',x/=10;
+    while(cnt--)pc(q[cnt]);
+}
+template<class T> inline T  Omin(T &a,T &b) {return a=min(a,b);}
+template<class T> inline T  Omax(T &a,T &b) {return a=max(a,b);}
+template<class T> inline T  Osqr(T a) {return sqrt((a));}
+
+using uint=unsigned int;
+using int64=long long;
+using uint64=unsigned long long;
+using ipair=std::pair<int,int>;
+using VI=std::vector<int>;
+using VD=std::vector<double>;
+using VVI=std::vector<VI>;
+
+const double pi=acos(-1.0);
+const double eps=1e-11;
+//========================================================
+int main()
+{
+#ifdef LOCAL
+	freopen("in.txt","r",stdin);
+	freopen("out.txt","w",stdout);
+#endif
+	clock_t stime = clock();
+	//========================================================
+	int n = read<int>(), p = read<int>();
+	int64 inv[n + 1];
+	inv[0] = 0;
+	inv[1] = 1;
+	for(int i = 2; i <= n; ++i)
+	{
+		int j = p % i, k = p / i;
+		inv[i] = (int64(p - k) * inv[j]) % p;
+	}
+	for(int i = 1; i <= n; ++i) printf("%lld\n", inv[i]);
+	//========================================================
+	std::cerr << "Time:" << clock() - stime << std::endl; 
+	return 0;
+}
--- 7.DStruct/README.md
+++ 7.DStruct/README.md
@@ -2,3 +2,5 @@
 - 20.8.10 creat -> tree_v
 - 20.8.11 creat -> ODT
 - 20.8.12 creat -> segT
+- 20.8.13 creat -> List 
+- 20.8.16 creat -> persistable_tree 
