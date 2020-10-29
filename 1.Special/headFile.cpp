7360101087d7e80cec9e87bde5daa68cf7a2bbd8
Tiooo111 <33785995@qq.com>
2020-10-29 09:16:35 +0800

    update headFiel

--- 1.Special/headFile.cpp
+++ 1.Special/headFile.cpp
@@ -1,54 +1,45 @@
-//#define LOCAL
-#include <iostream>
-#include <cstdio>
-#include <ctime>
-#include <cctype>
-#include <vector>
+﻿#include <iostream>
+#include <iomanip>
 #include <cmath>
-#include <cstring>
-
-#define OMID (r-l>>1)+l
-#define LOWBIT(i) (i)&(-i)
-#define REP(i,k) for(int i=0;i<(k);++i)
-#define FOR(i,j,k) for(int i=(j);i<(k);++i)
+#include <ctime>
+//========================================================
+#define IOS std::ios::sync_with_stdio(false);std::cin.tie(0);std::cout.tie(0);
+#define FOR(i, j, k) for(int (i) = (j); (i) <= k; (i)+=1)
+#define REP(i, j) for(int (i) = 0; (i) < j; (i)+=1)
+#define MID(l, r) (((r)-(l)>>1)+l)
+#define LOWBIT(i) ((i)&(-i))
+#define PA(idx) ((idx)>>1)
+#define LC(idx) ((idx)<<1)
+#define RC(idx) ((idx)<<1|1)
+#define ALL(A) A.begin(),A.end()
 #define MP make_pair
 #define PB push_back
 #define PP pop_back
-#define SIZE(A) ((int)A.size())
-#define ALL(A) A.begin(),A.end()
-#define PA ((idx)>>1)
-#define LC ((idx)<<1)
-#define RC ((idx)<<1|1)
-#define ch() getchar()
-#define pc(x) putchar(x)
-
-template<class T> inline T  Omin(T &a,const T &b) {return a<b?a:b;}
-template<class T> inline T  Omax(T &a,const T &b) {return a>b?a:b;}
-template<class T> inline T  Osqr(const T &a) {return sqrt((a));}
-
-using u32=unsigned int;
-using i64=long long;
-using u64=unsigned long long;
-using ipair=std::pair<int,int>;
-using VI=std::vector<int>;
-using VD=std::vector<double>;
-using VVI=std::vector<VI>;
-
-const double pi=acos(-1.0);
-const double eps=1e-11;
-
-template<typename T>inline T read(){
-    i64 f, x;char c;
-    for(f=1,c=ch();!isdigit(c);c=ch())if(c=='-')f=-f;
-    for(x=0;isdigit(c);c=ch())x=x*10+(c&15);
-	return x*f;
+//========================================================
+using ll = long long;
+using ull = unsigned long long;
+using lll = __int128;
+using ulll = unsigned __int128;
+//========================================================
+template<class T> inline void read(T &x) 
+{
+	int f; char c = getchar();
+	for(f = x = 0; (c < 48 || c > 57) && c > 0; c = getchar()) if(c == '-') f = 1;
+	for(;c > 47 && c < 58; c = getchar()) x = (x << 3)+(x << 1)+(c & 15);
+	if (f) x = -x;
 }
-template<typename T>inline void print(T x){
-    static char q[64];int cnt=0;
-    if(!x)pc('0');if(x<0)pc('-'),x=-x;
-    while(x)q[cnt++]=x%10+'0',x/=10;
-    while(cnt--)pc(q[cnt]);
+template<class T,class... Args> inline void read(T &x, Args&... args) { read(x), read(args...); }
+template<class T>inline void write(T x)
+{
+    static char q[130]; int cnt = 0;
+    if(!x) putchar('0'); if(x<0) putchar('-'),x = -x;
+    while(x) q[cnt++]=x % 10 + 48, x /= 10;
+    while(cnt--) putchar(q[cnt]);
 }
+template<class T>inline void write(T x, char c) { write(x); putchar(c); }
+//========================================================
+const double pi = acos(-1.0);
+const double eps = 1e-6;
 //========================================================
 int main()
 {
@@ -56,9 +47,10 @@ int main()
 	freopen("in.txt","r",stdin);
 	freopen("out.txt","w",stdout);
 #endif
+	IOS
 	clock_t stime = clock();
 	//========================================================
-	
+
 	//========================================================
 	std::cerr << "Time:" << clock() - stime << std::endl; 
 	return 0;
